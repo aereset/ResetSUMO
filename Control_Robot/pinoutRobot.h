d7fa0be7233308bddcfbe2d7139f3d6000467d1d
JaimeBravoAlgaba <jaime.bravo.algaba@gmail.com>
2023-02-20 19:32:22 +0100

    Configuraciín general

---
+++ Control_Robot/pinoutRobot.h
@@ -0,0 +1,15 @@
+// Pinout Robot
+
+//Pins right motor
+#define RM 7 
+#define RMB 6 
+#define PWMR 5 
+
+//Pins left motor
+#define LM 10 
+#define LMB 8 
+#define PWML 9
+
+//Pins for RF module data transmission (analog out)
+#define CE 4
+#define CSN 3
